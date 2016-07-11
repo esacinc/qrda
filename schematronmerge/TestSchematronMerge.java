2061a4dd145f2a377b87be8bff02dab641250a49
shonvick <shon.vick@esacinc.com>
2016-07-11 12:56:56 -0400

    testing files

---
+++ schematronmerge/TestSchematronMerge.java
@@ -0,0 +1,18 @@
+package schemaVerification;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSchematronMerge {
+
+	@Test
+	public void test() {
+		
+		//TODO
+		// Try a set of schematron files
+
+		assertTrue(true);
+	}
+
+}
